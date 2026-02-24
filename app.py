6d91d288aa0caf9dfdb068f4f1a356a97f2f4884
Riteshadwani1982 <117441941+Riteshadwani1982@users.noreply.github.com>
2026-02-24 22:09:06 +0530

    Replace load_qa_chain with RetrievalQA import

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import streamlit as st
 import google.generativeai as genai
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.question_answering import load_qa_chain
+from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
