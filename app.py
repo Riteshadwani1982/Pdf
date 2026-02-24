6638d02ab732d34e04be6d98436a2db5bd5c8a26
Riteshadwani1982 <117441941+Riteshadwani1982@users.noreply.github.com>
2026-02-24 23:13:17 +0530

    Update import statements for langchain modules

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
 from PyPDF2 import PdfReader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import GoogleGenerativeAIEmbeddings
 import streamlit as st
 import google.generativeai as genai
-from langchain_community.vectorstores import FAISS
+from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
